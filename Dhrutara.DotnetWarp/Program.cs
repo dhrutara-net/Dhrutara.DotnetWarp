e8735932f8c63758c2464b1e272d1f274fcbf5b8
Ramesh Kanjinghat <RameshK@kaplantrucking.com>
2020-05-23 13:51:54 -0400

    Code and README.md

---
+++ Dhrutara.DotnetWarp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dhrutara.DotnetWarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Purpose
+
+A sample project that uses dotnet-warp with Azure Devops CI/CD pipeline.
+
+Check https://github.com/Hubert-Rybak/dotnet-warp more details on dotnet-warp.
